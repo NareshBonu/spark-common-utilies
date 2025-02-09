786af5244020fa5a9be502bd4abc4c406f24e75c
NareshBonu <naresh.kumar13@gmail.com>
2025-02-09 23:31:01 +0530

    updated SCD2 logic

--- src/main/java/SCD2/SCD2Implemention.java
+++ src/main/java/SCD2/SCD2Implemention.java
@@ -1,5 +1,6 @@
 package SCD2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.expressions.WindowSpec;
@@ -12,6 +13,7 @@ import util.SparkCommons;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 public class SCD2Implemention {
     private static SparkSession sparkSession;
     public static void main(String[] args){
@@ -24,32 +26,41 @@ public class SCD2Implemention {
         Column join_Condition = masterDF.col("ID").equalTo(deltaDF.col("ID")) ;
         Dataset<Row> joinedDF = deltaDF.join(masterDF, join_Condition, "left_outer");
         joinedDF.show();
-        //Filter for new or changed records
-        Dataset<Row> changed_or_newDF = joinedDF.filter(masterDF.col("ID").isNull().or(
-                masterDF.col("CITY").notEqual(deltaDF.col("CITY"))));
+        //Filter for New Record
 
+        Dataset<Row> new_records_DF  = joinedDF.filter(masterDF.col("ID").isNull());
+        new_records_DF = new_records_DF.select(deltaDF.col("ID"),deltaDF.col("NAME"),deltaDF.col("CITY"));
+        //Filter for Updated Record
+        Dataset<Row> updated_records_DF = joinedDF.filter(masterDF.col("CITY").notEqual(deltaDF.col("CITY")));
+        updated_records_DF = updated_records_DF.select(deltaDF.col("ID"),deltaDF.col("NAME"),deltaDF.col("CITY"));
 
-        changed_or_newDF =  changed_or_newDF.select(deltaDF.col("ID"),deltaDF.col("NAME"),deltaDF.col("CITY")) ;
-        Dataset<Row> new_recordsDF =  changed_or_newDF
+        Dataset<Row> new_and_updated_recordsDF = new_records_DF.union(updated_records_DF) ;
+        new_and_updated_recordsDF =  new_and_updated_recordsDF
                 .withColumn("EFF_DT", functions.date_format(functions.current_date(),"yyyy-MM-dd"))
                 .withColumn("END_DT", functions.lit("9999-12-31"))
                 .withColumn("ACT_IN", functions.lit("Y")) ;
-        new_recordsDF.show();
+        log.info("New and Updated Records---------------------");
+        new_and_updated_recordsDF.show();
+
         //Mark old records as inactive
-        Dataset<Row>    inactive_recordsDF = masterDF.join(changed_or_newDF.select("id").distinct(), "ID", "inner");
-        inactive_recordsDF.show();
+        Dataset<Row>    inactive_recordsDF = masterDF.join(new_and_updated_recordsDF.select("id").distinct(), "ID", "inner");
         inactive_recordsDF =inactive_recordsDF
                 .withColumn("END_DT", functions.date_format(functions.current_date(),"yyyy-MM-dd"))
-                .withColumn("ACT_IN", functions.lit("N"))
-                ;
+                .withColumn("ACT_IN", functions.lit("N"));
+        log.info("INACTIVE RECORDS---------------------");
         inactive_recordsDF.show();
-        Dataset<Row>  finalDF = masterDF.union(new_recordsDF).union(inactive_recordsDF);
-        finalDF = finalDF.orderBy(finalDF.col("ID"));
-        finalDF.show();
 
-        WindowSpec window = Window.partitionBy("ID","ACT_IN").orderBy(finalDF.col("EFF_DT").desc()) ;
+        // Master DF + New Records + Updated Records + Inactive Records
+        Dataset<Row>  all_records_DF = masterDF.union(new_and_updated_recordsDF).union(inactive_recordsDF);
+        all_records_DF = all_records_DF.orderBy(all_records_DF.col("ID"));
 
-        finalDF = finalDF.withColumn("row_num", functions.row_number().over(window))
+        log.info("FINAL Records---------------------");
+        all_records_DF.show();
+
+        //Remove old updated record from MASTER DF
+        WindowSpec window = Window.partitionBy("ID","ACT_IN").orderBy(all_records_DF.col("EFF_DT").desc()) ;
+
+        Dataset<Row> finalDF = all_records_DF.withColumn("row_num", functions.row_number().over(window))
                 .filter(new Column("row_num").equalTo(1)).drop("row_num") ;
 
         finalDF.show();
